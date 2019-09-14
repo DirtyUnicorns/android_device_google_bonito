6f4d33bcf0483f1f1f9bcba19da7285fafa0b160
Josh Chasky <jchasky@gmail.com>
2019-09-14 01:25:24 -0400

    b4: DU-ify!
    Change-Id: I9470f1fb6c98fc6b248f3f2c0214beebb8b8b0b2

---
+++ du.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2018-2019 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include DU common configuration
+include vendor/du/config/common_full_phone.mk
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/bonito/aosp_bonito.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+PRODUCT_NAME := du_bonito
+PRODUCT_DEVICE := bonito
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3a XL
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="bonito" \
+    PRIVATE_BUILD_DESC="bonito-user 10 QQ3A.200705.002 6506677 release-keys"
+
+BUILD_FINGERPRINT := "google/bonito/bonito:QQ3A.200705.002/6506677:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/bonito/bonito:10/QQ3A.200705.002/6506677:user/release-keys
+
+BOARD_PREBUILT_VENDORIMAGE := vendor/images/bonito/vendor.img
+
+$(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
